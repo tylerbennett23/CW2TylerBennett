6bac7a2e9fb3b04713f7f7e47326851bfd5d1209
(s) Tyler Bennett <tlbennett@SOCNET.SOC.PLYMOUTH.AC.UK>
2024-01-03 17:30:45 +0000

    First attempt in the api

---
+++ My First API/test1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace test1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+       
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            //var rng = new Random();
+            return Enumerable.Range(1, 1).Select(index => new WeatherForecast
+            {
+                Email = "grace@plymouth.ac.uk",
+                Password = "ISAD123!"
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ My First API/test1/WeatherForecast.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace test1
+{
+    public class WeatherForecast
+    {
+               
+        public string Email{ get; set; }
+        public string Password{ get; set; }
+
+
+    }
+}
